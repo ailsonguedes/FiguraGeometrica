e16a50e5e71264eeb125ac6be63c39fc292e458d
ailsonguedes <ailsonsixseven@gmail.com>
2023-03-20 14:57:36 -0300

    First Commit

---
+++ src/Circulo.java
@@ -0,0 +1,32 @@
+public class Circulo extends FiguraGeometrica {
+	
+	private double pi = 3.14;
+	private double raio = 3;
+	
+	public String getNome(){
+		return "Ciruclo";
+	}
+	
+	public String getDescricao(){
+		return getNome() + ": Pi= "+getPi()+ " e Raio="+getRaio() ;
+	}
+	
+	public double getArea(){
+		return getPi() * Math.pow(getRaio(), 2);
+	}
+	
+	public double getPi() {
+		return pi;
+	}
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+	
+	public double getRaio() {
+		return raio;
+	}
+	public void setRaio(double raio) {
+		this.raio = raio;
+	}
+
+}
